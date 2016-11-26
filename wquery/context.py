b8e1d98e52ced0ad0fa3b9176c69ccc5873a4dd4
Liang Feng <Liang Feng>
2016-11-26 16:08:00 +0200

    更新context方案;加入编辑器快捷键hook

--- wquery/context.py
+++ wquery/context.py
@@ -4,9 +4,6 @@ import sys
 from collections import defaultdict
 savepath = os.path.join(sys.path[0], 'config')
 
-focus_editor = None
-focus_browser = None
-focus_note = None
 maps = list()
 model_id = -10000
 # [model_id: maps]
@@ -14,8 +11,4 @@ mappings = defaultdict(list)
 
 update_all = False
 
-
-def update_maps():
-    global model_id, maps
-    model_id = focus_editor.model()['id']
-    maps = mappings[model_id]
+context = defaultdict(str)
