37674f9bfcc9d226ecbbb3b826f24db726604704
yu7777 <31005010+yu7777@users.noreply.github.com>
2017-08-14 20:29:21 +0800

    Revise LDOCE6.py
    Add new dic fields

--- wquery/service/LDOCE6.py
+++ wquery/service/LDOCE6.py
@@ -1,13 +1,16 @@
 #-*- coding:utf-8 -*-
 import re
+import random
+
 
 from aqt.utils import showInfo, showText
-from base import MdxService, export, register, with_styles
 
-path = u'D:\\dicts\\LDOCE\\LDOCE6.mdx'
+from .base import export, with_styles, register, MdxService
+
+path = u'/Users/yu/Documents/english study/mdx/LDOCE6双解/L6mp3.mdx'
 
 
-@register(u'本地词典解析Sample-LDOCE6')
+@register(u'本地词典-LDOCE6')
 class Ldoce6(MdxService):
 
     def __init__(self, dict_path):
@@ -28,3 +31,58 @@ class Ldoce6(MdxService):
         if m:
             return m.groups()[0]
         return ''
+
+    @export(u'Bre单词发音', 2)
+    def fld_voicebre(self):
+        html = self.get_html()
+        m = re.search(r'<span class="brevoice">(.*?)</span brevoice>', html)
+        if m:
+            return m.groups()[0]
+        return ''
+		
+    @export(u'Ame单词发音', 3)
+    def fld_voiceame(self):
+        html = self.get_html()
+        m = re.search(r'<span class="amevoice">(.*?)</span amevoice>', html)
+        if m:
+            return m.groups()[0]
+        return ''
+
+    @export(u'sentence', 4)
+    def fld_sentence(self):
+        html = self.get_html()
+        m = re.search(r'<span class="example">(.*?)</span example>', html)
+        if m:
+            return re.sub('<img.*?png">','',m.groups()[0])
+        return ''
+		
+    @export(u'def', 5)
+    def fld_definate(self):
+        html = self.get_html()
+        m = re.search(r'<span class="def">(.*?)</span def>', html)
+        if m:
+            return m.groups()[0]
+        return ''
+		
+    @export(u'random_sentence', 6)
+    def fld_random_sentence(self):
+        html = self.get_html()
+        m = re.findall(r'<span class="example">(.*?)</span example>', html)
+        if m:
+            number=len(m)
+            index = random.randrange(0,number-1,1)
+            return re.sub('<img.*?png">','',m[index])
+        return ''
+
+    @export(u'all sentence', 7)
+    def fld_allsentence(self):
+		html = self.get_html()
+		m=re.findall(r'(<span class="example">.+?</span example><span class="example_c">.+?</span example_c>)',html)
+		if m:
+			items=0
+			my_str=''
+			for items in range(len(m)):
+				my_str=my_str+m[items]
+			return my_str
+		return ''
+	
---
+++ wquery/service/txt.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 -*-
+import re
+
+from aqt.utils import showInfo, showText
+from base import TxtService, export, register, with_styles
+
+path = u'/Users/yu/Downloads/q/aaa.txt'
+
+
+@register(u'txt测试')
+class TxtTest(TxtService):
+
+    def __init__(self):
+        super(TxtTest, self).__init__(path)
+
+    # @property
+    # def unique(self):
+    #     return self.__class__.__name__
+
+    @export(u'例句', 1)
+    def fld_sentence(self):
+        with open(self.dict_path, 'rb') as f:
+            lines = f.readlines()
+            for line in lines: 
+                line = line.decode("UTF-8")
+                m=re.search(self.word,line)       
+                if m:
+                    return line
+        return ''
+
