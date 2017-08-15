e6ea60f37c2251dd74b6991e55d14bd597b4c8d5
finalion <finalion@gmail.com>
2017-08-15 11:45:52 +0300

    update txtservice demo

--- wquery/service/txt.py
+++ wquery/service/txt.py
@@ -2,29 +2,27 @@
 import re
 
 from aqt.utils import showInfo, showText
-from base import TxtService, export, register, with_styles
+from base import LocalService, export, register, with_styles
 
-path = u'/Users/yu/Downloads/q/aaa.txt'
+path = u'D:\\dicts\\LDOCE\\d.txt'
 
 
 @register(u'txt测试')
-class TxtTest(TxtService):
+class TxtTest(LocalService):
 
     def __init__(self):
         super(TxtTest, self).__init__(path)
+        try:
+            self.handle = open(path, 'rb')
+        except:
+            self.handle = None
 
-    # @property
-    # def unique(self):
-    #     return self.__class__.__name__
-
-    @export(u'自制例句', 1)
-    def fld_sentence(self):
-        with open(self.dict_path, 'rb') as f:
-            lines = f.readlines()
-            for line in lines: 
-                line = line.decode("UTF-8")
-                m=re.search(self.word,line)       
-                if m:
-                    return line
-        return ''
-
+    @export(u'all', 1)
+    def fld_phonetic(self):
+        if not self.handle:
+            return
+        for line in self.handle:
+            line = line.decode("UTF-8")
+            m = re.search(self.word, line)
+            if m:
+                return line
